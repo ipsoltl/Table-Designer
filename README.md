b7b9979406ba7d2656afe5bb379c2e5ede6a35d4
ipsoltl <ipsoltl@gmail.com>
2018-02-09 16:24:34 +0300

    Первый выпуск v0.1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Table-Designer
+# Конструктор таблиц
+### Практическое задание по изучению javascript и работе с github.
+#### Практика работы
+
+Пока с помощью данного конструктора можно выбрать количество стобцов, строк и получить HTML код таблицы.
+
+##### 0.1 версия
+* document.createTextNode
+* appendChild
+* document.querySelectorAll
+* метод getElementById объекта Document
+* свойство innerHTML объекта Element
+* созданы функции: countNumberLines, launchingCodeHTML
+* созданы файлы script.js, index.html, style.css
---
+++ script.js
@@ -0,0 +1,35 @@
+function countNumberLines() {
+    var lines = document.getElementById('numberOfLines').value;
+    var columns = document.getElementById('numberOfColumns').value;
+
+    for (let index = 0; index < lines; index++) {
+        var tr = document.createElement('tr');
+        tr.id = 'tr'+ index;
+        document.getElementById('table').appendChild(tr);
+    
+        for (let index2 = 0; index2 < columns; index2++) {
+            var td = document.createElement('td');
+            td.innerHTML=index2;
+            td.id = 'td' + index2;
+            document.getElementById('tr' + index).appendChild(td);
+      
+        };
+        
+    };
+
+    var elements = document.querySelectorAll('td');
+    for (let index33 = 0; index33 < elements.length; index33++) {
+        proverca = index33 + 1;
+        elements[index33].innerHTML = proverca;
+        elements[index33].style.border = "ridge";
+        elements[index33].style.padding = "3px";
+          
+    };
+    
+};
+function launchingCodeHTML() {
+    document.getElementById('startCode').style.display = 'block';
+    var codeHtml = table.innerHTML;
+    samCode.appendChild(document.createTextNode(codeHtml));
+
+};
